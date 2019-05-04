09875de1d23a34cd29a715c1c7f64a45365d61ba
Alexandre Abreu <44318137+a3brx@users.noreply.github.com>
2019-05-04 18:41:28 +0100

    Main with new includes

--- NiceHolidays/main.cpp
+++ NiceHolidays/main.cpp
@@ -2,28 +2,24 @@
 #include <string>
 #include <vector>
 #include "utils.h"
-#include "Date.h"
-//#include "Agency.h"
-#include "Packet.h"
-//#include "Menus.h"
-#include "Table.h"
+#include "Date.cpp"
+//#include "Agency.cpp"
+#include "Packet.cpp"
+//#include "Menus.cpp"
+#include "Table.cpp"
+#include "Address.cpp"
 using namespace std;
 int main() {
 	//Agency agency(AGENCY_FILE_NAME);   					// create the agency
 	//mainMenu(agency); 									// initial menu inicial with the major options of the application
-	Date d1(20, 7, 1999);
-	Date d2;
-	cout << d2 << '\n';
-	cout << d2 - d1 << '\n';
-	Table t({"Key", "Change"}, {{"N", "Name"}, {"F", "Family Size"}, {"A", "Address"}, {"P", "Packs"}});
-	cout << t;
-	Date b(1, 8, 2019);
-	Date e(5, 8, 2019);
-	Packet p({"Madeira", "Funchal", "Porto Santo"}, b, e, 300, 50);
-	cout << p << '\n';
+	Table table({"Key", "Change"}, {{"N", "Name"}, {"F", "Family Size"}, {"A", "Address"}, {"P", "Packs"}});
+	Packet packet({"Madeira", "Funchal", "Porto Santo"}, Date(1, 8, 2019), Date(5, 8, 2019), 300, 50);
+	Address address("Rua Sem Fim", 200, "5Esq", "1200-001", "Lisboa");
+	cout << table << '\n' << packet << '\n' << address << '\n';
 	ofstream f;
 	f.open("test.txt");
-	f << p << '\n' << SEPARATOR << '\n';
+	f << packet << '\n' << SEPARATOR << '\n';
+	f << address;
 	f.close();
 	return 0;
 }
